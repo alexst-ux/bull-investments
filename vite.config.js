f65c5e5f4dd95c66101dec2249c2b4e0817ec832
OleksiyS <a.struchkov@gmail.com>
2024-09-24 18:02:24 +0200

    exclude path 'src/data/' from build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 //import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [path.resolve(__dirname, "src/data/**")],
+    },
+  },
   plugins: [react() /*, eslint()*/],
   /* esbuild: {
     supported: {
